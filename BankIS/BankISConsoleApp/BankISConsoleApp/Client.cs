60ccc7b5cfaa9fe58767eb6f6405dd9ceff648b0
zalpav <91120645+zalpav@users.noreply.github.com>
2021-09-21 12:18:01 +0200

    Změna1

---
+++ BankIS/BankISConsoleApp/BankISConsoleApp/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankIS.ConsoleApp
+{
+    public class Client
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ BankIS/BankISConsoleApp/BankISConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankIS.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Client c1 = new Client();
+
+            c1.Name = "Pepa Čech";
+            c1.Age = 43;
+
+            c1.Print();
+
+
+            Client c2 = new Client();
+            c2.Name = "Jan Nový";
+            c2.Age = 25;
+            c2.Print();
+
+            Console.ReadLine();
+
+        }
+    }
+}
